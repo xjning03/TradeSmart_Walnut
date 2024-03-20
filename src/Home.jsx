df57f4a376847c0ccaf17cc6432af8235383951e
JieYing <soojieying3617@gmail.com>
2024-03-21 00:22:23 +0800

    home.css file

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import './Home.css';
+
+function SearchBar() {
+    return (
+      <div className="search-bar">
+        <input type="text" placeholder="Search..." style={{ width: '250px' }}/>
+      </div>
+    );
+  }
 
 function Home() {
     return (
         <div className='Home' id='Home'>
-            <h1>Hello</h1>
+            <div className="search-bar-container">
+                <SearchBar />
+            </div>
         </div>
     );
 }
